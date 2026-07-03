1ab40c88b94653cc0325e0351ec3aa48b3e58366
Patrickx7 <p.jorgemoura@gmail.com>
2026-07-03 15:22:11 -0300

    Mudando a ilha php para o inicio, assim não existe a possibilidade de haver bugs.

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gerenciador de Tarefas</title>
-</head>
-
-<body>
-
-    <h1>Gerenciador de Tarefas</h1>
-
     <?php
 
     $nome = "Patrick";
@@ -34,38 +21,55 @@
     }
 
 
-    echo "<p>Meu nome é $nome! e eu amo $linguagem.</p>";
+    
 
     ?>
 
-    <form method="POST" action="index.php">
-        <input type="text" name="titulo" placeholder="Digite uma tarefa">
-        <button type="submit">Adicionar</button>
-    </form>
+    <!DOCTYPE html>
+    <html lang="pt-BR">
 
-    <h2>Minhas tarefas</h2>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Gerenciador de Tarefas</title>
+    </head>
 
-    <ul>
+    <body>
+
+        <h1>Gerenciador de Tarefas</h1>
+
         <?php
-
-        foreach ($tarefas as $tarefa) {
-            echo "<li>$tarefa</li>";
-        }
-
+        echo "<p>Meu nome é $nome! e eu amo $linguagem.</p>";
         ?>
-        
-    </ul>
-        
+
+        <form method="POST" action="index.php">
+            <input type="text" name="titulo" placeholder="Digite uma tarefa">
+            <button type="submit">Adicionar</button>
+        </form>
+
+        <h2>Minhas tarefas</h2>
+
+        <ul>
+            <?php
+
+            foreach ($tarefas as $tarefa) {
+                echo "<li>$tarefa</li>";
+            }
+
+            ?>
+
+        </ul>
+
         <?php
 
         $total = count($tarefas);           // pergunta e guarda a resposta na caixinha
-                 
+
         echo "<p>Você tem $total tarefas no total.</p>";   // usa a caixinha na frase
-        
+
         ?>
 
-    
 
-</body>
 
-</html>
+    </body>
+
+    </html>
